4796cf3c90cf883403ada6b1656e69e22ff5cec9
Adit <32681526+Aditmadzs@users.noreply.github.com>
2018-10-29 23:36:48 +0700

    Update adit.py

--- adit.py
+++ adit.py
@@ -4,7 +4,7 @@ from LINEPY import *
 from akad.ttypes import *
 from multiprocessing import Pool, Process
 from time import sleep
-import pytz, datetime, pafy, time, timeit, random, sys, ast, re, os, json, subprocess, threading, string, codecs, requests, tweepy, ctypes, urllib, wikipedia
+import pytz, datetime, pafy, time, timeit, random, sys, ast, re, os, json, subprocess, threading, string, codecs, requests, ctypes, urllib, wikipedia
 from datetime import timedelta, date
 from datetime import datetime
 from bs4 import BeautifulSoup
